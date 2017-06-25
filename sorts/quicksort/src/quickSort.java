491aa6c3508152f1f3d620241b3fafff8000f862
Jas1052 <jliu1052@gmail.com>
2017-06-25 10:43:41 -0400

    Quicksort random values

--- sorts/quicksort/src/quickSort.java
+++ sorts/quicksort/src/quickSort.java
@@ -1,21 +1,33 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by jas1052 on 6/24/17.
- */
+ **/
 public class quickSort {
     public static void main(String[] args){
-        int input;
+        int input = -1;
         Scanner reader = new Scanner(System.in);
-        try {
-            System.out.println("Input a number");
-            input = reader.nextInt();
-        }
-        catch(InputMismatchException e){
-            System.out.println("That's not an int. Enter an integer greater than 0");
-            input = reader.nextInt();
+        while(input < 1) {
+            try {
+                System.out.print("Input a valid number greater than 0: ");
+                input = reader.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("That's not an integer.");
+                reader.next();
+            }
         }
         System.out.println("You chose: " + input);
+        int[] numbers = new int[input];
+        for(int i = 0; i < input; i++){
+            //populates numbers with random integers between 0 and 100
+            numbers[i] = ThreadLocalRandom.current().nextInt(0, 100);
+        }
+        /*  
+        for(int number: numbers){
+            System.out.println(number);
+        }
+        */
     }
 }
