8c781ca284f84adeadf4ac28f9514aeee1b9db6f
Jas1052 <jliu1052@gmail.com>
2017-06-26 23:33:30 -0400

    comments

--- sorts/quicksort/src/quickSort.java
+++ sorts/quicksort/src/quickSort.java
@@ -6,10 +6,13 @@ import java.util.concurrent.ThreadLocalRandom;
  * Created by jas1052 on 6/24/17.
  **/
 public class quickSort {
-    public static void main(String[] args){
+    private static void quickRecur() {
+        return;
+    }
+    public static void main(String[] args) {
         int input = -1;
         Scanner reader = new Scanner(System.in);
-        while(input < 1) {
+        while (input < 1) {
             try {
                 System.out.print("Input a valid number greater than 0: ");
                 input = reader.nextInt();
@@ -20,7 +23,7 @@ public class quickSort {
         }
         System.out.println("You chose: " + input);
         int[] numbers = new int[input];
-        for(int i = 0; i < input; i++){
+        for (int i = 0; i < input; i++) {
             //populates numbers with random integers between 0 and 100
             numbers[i] = ThreadLocalRandom.current().nextInt(0, 100);
         }
