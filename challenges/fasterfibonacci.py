a44c6809a48901104450a087e549c77040579f15
Jas1052 <jliu1052@gmail.com>
2017-11-07 23:46:09 -0600

    increased recursion depth

--- challenges/fasterfibonacci.py
+++ challenges/fasterfibonacci.py
@@ -1,4 +1,7 @@
 import time
+import sys
+# sets maxixum recursion limit (for testing, very unrecommended)
+sys.setrecursionlimit(10000) 
 
 def fib(n):
     if n == 0:
@@ -20,10 +23,11 @@ def memFib(n, memo = {0:0, 1:1}):
         memo[n] = memFib(n-1, memo) + memFib(n-2, memo)
     return memo[n]
 
-depth = 100
+depth = 5000
 
 start = time.time()
 
+# for higher depths, don't forget to comment out fib(depth)
 print(fib(depth))
 fibTime = time.time()
 print(bottomUpFib(depth)[-1]) # gets last element
