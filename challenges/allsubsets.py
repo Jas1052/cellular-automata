8803e79dcd41f0a040f38624d2d4be3c2c953f8c
Jas1052 <jliu1052@gmail.com>
2017-11-08 15:54:27 -0600

    added matrix fib

--- challenges/allsubsets.py
+++ challenges/allsubsets.py
@@ -1,9 +1,8 @@
+# finds all subset of a list
 def subset(numbers):
     if numbers == []:
         return [[]]
-
     x = subset(numbers[1:])
-
     return x + [[numbers[0]] + y for y in x]
 
 print(subset([1, 2, 3]))
--- challenges/fasterfibonacci.py
+++ challenges/fasterfibonacci.py
@@ -1,7 +1,7 @@
 import time
 import sys
 # sets maxixum recursion limit (for testing, very unrecommended)
-sys.setrecursionlimit(10000) 
+sys.setrecursionlimit(100000) 
 
 def fib(n):
     if n == 0:
@@ -23,18 +23,30 @@ def memFib(n, memo = {0:0, 1:1}):
         memo[n] = memFib(n-1, memo) + memFib(n-2, memo)
     return memo[n]
 
-depth = 5000
+# I did not write this method; pulled from geeksforgeeks.org
+def matrixFib(n):
+    v1, v2, v3 = 1, 1, 0    # initialise a matrix [[1,1],[1,0]]
+    for rec in bin(n)[3:]:  # perform fast exponentiation of the matrix (quickly raise it to the nth power)
+        calc = v2*v2
+        v1, v2, v3 = v1*v1+calc, (v1+v3)*v2, calc+v3*v3
+        if rec=='1':    v1, v2, v3 = v1+v2, v1, v2
+    return v2
+
+depth = 10000
 
 start = time.time()
 
 # for higher depths, don't forget to comment out fib(depth)
-print(fib(depth))
+# print(fib(depth))
 fibTime = time.time()
-print(bottomUpFib(depth)[-1]) # gets last element
+# print(bottomUpFib(depth)[-1]) # gets last element
 bottomUpTime = time.time()
-print(memFib(depth))
+# print(memFib(depth))
 memTime = time.time()
+print(matrixFib(depth))
+matrixTime = time.time()
 
 print("Fib Time: " + str(fibTime-start))
 print("Bottom-Up Time: " + str(bottomUpTime-fibTime))
 print("Memoization Time: " + str(memTime-bottomUpTime))
+print("Matrix Time: " + str(matrixTime-memTime))
